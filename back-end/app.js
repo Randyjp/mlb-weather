f73cb51bd4801bbf7dde8e6cb713eb8ddaddeeef
Randy Perez <randyperez@Intellisyss-Mac-mini-7.local>
2018-04-02 15:55:46 -0400

    adding mongoose

--- back-end/app.js
+++ back-end/app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const moongoose = require('mongoose');
+
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
