14e324d5bc8150eadf197c72bebb9daec4c5bdd7
Randy Perez <randyjp125@gmail.com>
2018-04-12 15:00:00 -0400

    testing the footer

--- front-end/src/tests/components/Footer.test.js
+++ front-end/src/tests/components/Footer.test.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import {shallow} from 'enzyme';
+import {GridColumn} from 'semantic-ui-react';
+
 import Footer from '../../components/Footer';
 
 it('renders without crashing', () => {
     shallow(<Footer />);
+});
+
+it('should render three <GridColumn> components', () => {
+    const footer = shallow(<Footer/>);
+    expect(footer.find(GridColumn)).toHaveLength(3);
 });
