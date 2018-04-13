3a2beeec6b214f282902ad95a1360fc10c705d1b
Randy Perez <randyjp125@gmail.com>
2018-04-13 15:24:11 -0400

    testing main component, still work in progress

--- front-end/src/setupTests.js
+++ front-end/src/setupTests.js
@@ -1,4 +1,35 @@
 import {configure} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import { JSDOM }  from 'jsdom';
+import fetch from 'jest-fetch-mock';
 
 configure({adapter: new Adapter()});
+
+const jsdom = new JSDOM('<!doctype html><html><body></body></html>');
+const { window } = jsdom;
+
+const localStorageMock = {
+    getItem: jest.fn(),
+    setItem: jest.fn(),
+    clear: jest.fn()
+};
+
+function copyProps(src, target) {
+    const props = Object.getOwnPropertyNames(src)
+        .filter(prop => typeof target[prop] === 'undefined')
+        .reduce((result, prop) => ({
+            ...result,
+            [prop]: Object.getOwnPropertyDescriptor(src, prop),
+        }), {});
+    Object.defineProperties(target, props);
+}
+
+
+global.localStorage = localStorageMock;
+global.fetch = fetch;
+global.window = window;
+global.document = window.document;
+global.navigator = {
+    userAgent: 'node.js',
+};
+copyProps(window, global);
