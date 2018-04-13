87c63941224cb556f5c8f2a534d1f52b425b632f
Randy Perez <randyjp125@gmail.com>
2018-04-13 11:14:01 -0400

    addign snapshot test to unit selector

--- front-end/src/tests/components/UnitSelector.test.js
+++ front-end/src/tests/components/UnitSelector.test.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import {shallow} from 'enzyme';
 import {Button} from 'semantic-ui-react';
+import renderer from 'react-test-renderer';
 
 import {UnitSystem} from '../../enums';
-import  UnitSelector from '../../components/UnitSelector';
+import UnitSelector from '../../components/UnitSelector';
 
 it('renders without crashing', () => {
-    shallow(<UnitSelector units={UnitSystem.METRIC} handleUnitChange={() => {}}/>);
+    shallow(<UnitSelector units={UnitSystem.METRIC} handleUnitChange={() => {
+    }}/>);
+});
+
+it('should render correctly', () => {
+    const tree = renderer.create(<UnitSelector handleUnitChange={()=>{}}
+                                        units={UnitSystem.IMPERIAL}/>);
+    expect(tree.toJSON()).toMatchSnapshot();
+
 });
 
 describe('<UnitSelector/> component', () => {
@@ -22,13 +31,22 @@ describe('<UnitSelector/> component', () => {
 
     });
 
+
     it('should render two <Button> components', () => {
         expect(wrapper.find(Button)).toHaveLength(2);
     });
 
-    it('simulates click events', () =>{
+    it('simulates click events', () => {
         wrapper.find(Button).first().simulate('click');
         expect(spy).toHaveBeenCalledTimes(1);
+    });
+
+    it('should have active class on right button ', () => {
+        const first = wrapper.find(Button).first();
+        const second = wrapper.find(Button).last();
+
+        expect(first.props().active).toBeTruthy();
+        expect(second.props().active).toBeFalsy();
     });
 });
 
